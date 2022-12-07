a0ca7db21fa81a597402d538cb5661b057ef2744
Jprimero15 <jprimero155@gmail.com>
2022-12-08 00:41:44 +0800

    initial YAAP a13 bringup
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi439.mk
+    $(LOCAL_DIR)/yaap_Mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng
+    yaap_Mi439-user \
+    yaap_Mi439-userdebug \
+    yaap_Mi439-eng
