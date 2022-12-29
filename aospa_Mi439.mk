1ee517492d2894f0f2df536b5b281bc0e673a371
Jprimero15 <jprimero155@gmail.com>
2022-12-29 16:03:25 +0800

    mi439: imorove target mk

--- aospa_Mi439.mk
+++ aospa_Mi439.mk
@@ -1,35 +1,43 @@
+# Copyright (C) 2022 Paranoid Android
 #
-# Copyright (C) 2021 The LineageOS Project
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# SPDX-License-Identifier: Apache-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifeq (aospa_Mi439, $(TARGET_PRODUCT))
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common AOSPA stuff.
+# Inherit from Mi439 device configuration.
+$(call inherit-product, device/xiaomi/Mi439/device.mk)
+
+# Inherit from common AOSPA configuration
 $(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
-# Inherit from Mi439 device
-$(call inherit-product, device/xiaomi/Mi439/device.mk)
+PRODUCT_NAME := aospa_Mi439
+PRODUCT_DEVICE := Mi439
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI SDM439
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Overlays
 PRODUCT_PACKAGES += \
     xiaomi_pine_overlay_lineage \
     xiaomi_olive_overlay_lineage
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Mi439
-PRODUCT_NAME := aospa_Mi439
-BOARD_VENDOR := Xiaomi
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := SDM439
-PRODUCT_MANUFACTURER := Xiaomi
-TARGET_VENDOR := Xiaomi
-
-TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -38,3 +46,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys"
+
+endif
